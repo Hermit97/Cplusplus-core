51a9d8b1c7cfc0859d5edef47ebdf9039d63cd1e
ShawnAli97 <shawn10ali@gmail.com>
2022-02-12 16:54:07 -0500

    Fixed file location

---
+++ ch3/num4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+int main(){
+    std::string w1, w2, w3;
+
+    std::cout << "Enter three words\n";
+    std::cin >> w1 >> w2 >> w3;
+
+
+
+}
