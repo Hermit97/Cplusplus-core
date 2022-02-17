a6c646cba55a72c791b3580fa195915b8aaa4642
ShawnAli97 <shawn10ali@gmail.com>
2022-02-16 22:09:13 -0500

    Euro and pound conversions added

--- ch4/ex1.cpp
+++ ch4/ex1.cpp
@@ -5,10 +5,10 @@
 #include <cmath>
 
 int main(){
-    double yen = 0.0087;
+    float yen = 0.0087;
     double euro = 1.14;
     double pound = 1.36;
-    double yenToDollar, euroToDollar, poundToDollar, amount;
+    float yenToDollar, euroToDollar, poundToDollar, amount;
     std::string curr = " ";
 
     std::cout << "Enter the currency you want to get exchanged to usd. \n";
@@ -20,6 +20,14 @@ int main(){
     if(curr == "yen" || curr == "Yen"){
         yenToDollar = yen * amount;
         std::cout << yenToDollar << "\n";
+    }else if(curr == "euro" || curr == "Euro"){
+        euroToDollar = euro * amount;
+        std::cout << euroToDollar << "\n";
+    }else if(curr == "pound" || curr == "Pound"){
+        poundToDollar = pound * amount;
+        std::cout << poundToDollar << "\n";
     }
+
+    return 0;
 
 }
