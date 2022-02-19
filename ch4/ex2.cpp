9698263321c1f758d49acfdba146208e0c93e9cc
ShawnAli97 <shawn10ali@gmail.com>
2022-02-18 20:45:16 -0500

    Ex2

---
+++ ch4/ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+int main(){
+    int num = 1;
+    char letter = 'a';
+    int newLetter {'a' + num};
+
+    while(letter <= 'z'){
+       // std::cout <<
+    }
+
+}
