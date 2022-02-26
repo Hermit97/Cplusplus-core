39eb85759d1e5aed437f0413d86dd216301e54c7
ShawnAli97 <shawn10ali@gmail.com>
2022-02-26 16:15:00 -0500

    Finished ex4

---
+++ ch4/ex4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+void sqr(int n){
+
+    int start = 0;
+    for(int i = 0; i < n; i++){
+        //int start = 0;
+        start = start + n;
+    }
+    std::cout << start;
+}
+
+int main(){
+    sqr(4);
+
+
+}
