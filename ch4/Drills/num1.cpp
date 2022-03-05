4737c1acb654be59faf0217d67471eebfadac596
ShawnAli97 <shawn10ali@gmail.com>
2022-03-04 20:04:17 -0500

    Changed the types of num1 and num2 to ints instead of strings. Got rid of the if statments and changed the while loop

--- ch4/Drills/num1.cpp
+++ ch4/Drills/num1.cpp
@@ -7,20 +7,17 @@
 using namespace std;
 
 int main(){
-    std::string num1 {0};
-    std::string num2 {0};
+    //std::string num1 {0};
+    //std::string num2 {0};
 
+    int num1, num2;
 
     std::cout << "Enter two numbers\n";
-    std::cin >> num1 >> num2;
 
-    while(num1 != "|" || num2 != "|"){
-        std::cout << "Enter two numbers\n";
-        std::cin >> num1 ;
-        if(num1 == "|" || num2 == "|")
-            break;
-        std::cin >> num2;
-
+    //while(num1 != "|" || num2 != "|"){
+    while(std::cin >> num1 >> num2){
+       std::cout << "Num1: " << num1 << "\n num2: " << num2 << "\n";
     }
+    return 0;
 
 }
