fc378e0fda4c64744f6949e9a8a03b9c5a592760
ShawnAli97 <shawn10ali@gmail.com>
2022-03-07 17:00:16 -0500

    Finished num7

--- ch4/Drills/t2.cpp
+++ ch4/Drills/t2.cpp
@@ -5,22 +5,42 @@
 #include <cmath>
 #include <cstdlib>
 
-//Combine all of the problems. Also do the same for
-//Also for number 7 do actual research on the conversoin instead of trying to free style it
+/*Add a unit to each double entered; that is, enter values such as 10cm,
+2.5in, 5ft, or 3.33m. Accept the four units: cm, m, in, ft.
+Assume conversion factors 1m == 100cm, 1in == 2.54cm, 1ft == 12in. Read the unit
+indicator into a string. You may consider 12 m (with a space between the
+number and the unit) equivalent to 12m (without a space).Instead of trying to free style it
+*/
 int main(){
-
     double num1;
     double small = 0;
     double big = 0;
+    double convert = 0;
     std::string addUnit;
     bool isTrue = true;
     bool firstNum = true;
-    std::vector<std::string> units = {"cm", "m", "in", "ft"};
+    std::vector<std::string> units = {"cm", "in", "m", "ft"};
 
-    //How to stop the program? with | or something else but rather |??
-    //Need input to end once num1 enters 0 instead of asking for addUnit input
     std::cout << "Enter a number\n";
-    while(std::cin >> num1){
+    while(std::cin >> num1 >> addUnit){
+
+        //Convert cm to meters
+        if(addUnit == units[0]){
+            convert = num1 / 100;
+            std::cout << num1 << units[0]  << " converted to "<< convert << "m\n";
+            //Convert in to cm
+        }else if(addUnit == units[1]){
+            convert = num1 * 2.54;
+            std::cout << num1 << units[1] << " converted to "<< convert << "cm\n";
+            //Convert m to ft
+        }else if(addUnit == units[2]){
+            convert = num1 * 3.281;
+            std::cout << num1 << units[2] << " converted to "<< convert << "ft\n";
+            //Convert ft to in
+        }else if(addUnit == units[3]){
+            convert = num1 * 12;
+            std::cout << num1 << units[3] << " converted to "<< convert << "in\n";
+        }
 
         if(firstNum){
             small = num1;
