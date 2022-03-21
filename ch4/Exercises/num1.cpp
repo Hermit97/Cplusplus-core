f1bb522d11248a9037ae697dd2e66ebc6e15b4a2
ShawnAli97 <shawn10ali@gmail.com>
2022-03-20 20:39:38 -0400

    Get correct calculation for even number of data set for the median

--- ch4/Exercises/num1.cpp
+++ ch4/Exercises/num1.cpp
@@ -5,3 +5,17 @@
 #include <cmath>
 #include <cstdlib>
 #include <limits>
+
+int main() {
+    std::vector<double> temps;
+    for(double temp; std::cin >> temp;)
+        temps.push_back(temp);
+
+    double sum = 0;
+    for(int x : temps) sum += x;
+    std::cout << "Average temperature: " << sum/temps.size() << '\n';
+
+    sort(temps.begin(), temps.end());
+    std::cout << "Median temperature:" << temps[temps.size()/2] << '\n';
+
+}
