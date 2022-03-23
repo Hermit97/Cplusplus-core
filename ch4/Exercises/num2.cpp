2efd946ddc23bf5eb29a5f212b941ebf57dc8522
ShawnAli97 <shawn10ali@gmail.com>
2022-03-22 20:41:02 -0400

    Cleaned up folder

--- ch4/Exercises/num2.cpp
+++ ch4/Exercises/num2.cpp
@@ -6,44 +6,62 @@
 #include <cstdlib>
 #include <limits>
 
+/*Read a sequence of double values into a vector . Think of each value as
+the distance between two cities along a given route. Compute and print
+the total distance (the sum of all distances). Find and print the smallest
+and greatest distance between two neighboring cities. Find and print the
+mean distance between two neighboring cities.
+*/
+
 int main(){
-    std::vector<double> distances;
-    double small, big;
-    //distances.std::vector<double>::push_back(10);
+    std::vector<double> dists;
+    double mean;
 
-    //Read values into vector
-    for(double distance; std::cin >> distance;)
-        distances.push_back(distance);
+    std::cout << "Enter the distances\n";
+    for(double dist; std::cin >> dist;)
+        dists.push_back(dist);
 
-    //Print the vector
-    std::cout << "Vector: ";
+    std::cout << "Vector values:\n";
     std::string comma = " ";
-    for(int i = 0; i < distances.size(); i++){
-        std::cout << comma << distances[i];
+    for(int i = 0; i < dists.size(); i++){
+        std::cout << comma << dists[i];
         comma = ",";
     }
+
     std::cout << std::endl;
 
-    //Compute the total distance between the values entered
-    double sum = 0;;
-    for(int i = 0; i < distances.size(); i++)
-        sum += distances[i];
-
-    //Print the sum
-    std::cout << sum << std::endl;
-
-
-    //Try finding the smallest and largest without the sort function.
-        small = distances[0];
-        big = distances[0];
-        //100, 65, 88
-    for(int i = 1; i < distances.size(); i++){
-        if(distances[i] < small)
-            small = distances[i];
-
-        if(distances[i] > big)
-            big = distances[i];
+    //Total sum
+    int sum = 0;
+    for(int i = 0; i < dists.size(); i++){
+        sum += dists[i];
     }
 
-    std::cout << "Small value is " << small << " big value is " << big << "\n";
+    std::cout << "Sum = " << sum << "\n";
+
+    double small = dists[0];
+    double big = dists[0];
+
+    for(int i = 0; i < dists.size(); i++){
+        if(small > dists[i])
+            small = dists[i];
+        else if(big < dists[i])
+            big = dists[i];
+    }
+
+    std::cout << "Small: " << small << " Big: " << big << std::endl;
+
+    //Find the mean
+    int count = 1;
+    for(int i = 1; i < dists.size(); i++){
+        count = dists.size();
+    }
+    std::cout << count << "\n";
+
+    mean = sum / count;
+    std::cout << "Mean is " << mean << "\n";
+
+
+
+
+
 }
