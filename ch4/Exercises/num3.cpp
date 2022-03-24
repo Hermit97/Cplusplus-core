ac9a563eed34aae57fd5eda0f8d50b76a3b3375c
ShawnAli97 <shawn10ali@gmail.com>
2022-03-24 03:07:43 -0400

    Num3

--- ch4/Exercises/num3.cpp
+++ ch4/Exercises/num3.cpp
@@ -16,5 +16,46 @@ if-else construct.
 */
 
 int main(){
+    int small = 1;
+    int big = 100;
+    int usrNum;
+    char usrAnswer;
+    int maxQuestions = 7;
+    int count = 1;
+    //int count = 0;
+
+    std::cout << "Enter a number: \n";
+    std::cin >> usrNum;
+
+    //Check for invalid input
+    while(std::cin.fail()){
+        std::cout << "Not an int\n";
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        std::cout << "Enter another number: \n";
+        std::cin >> usrNum;
+
+    }
+    //Ask up to 7 questions
+    while(count <= maxQuestions){
+
+        //Question 1
+        std::cout << "Is the number you are thinking of above 50?\n";
+        std::cin >> usrAnswer;
+
+        while(usrAnswer != 'y' && usrAnswer != 'Y' && usrAnswer != 'n' && usrAnswer != 'N'){
+            std::cout << "Wrong input, try again.\n";
+            //std::cin.clear();
+            //std::cin.ignore(256, '\n');
+            std::cin >> usrAnswer;
+        }
+
+        //count++;
+        ++count;
+    }
+
+
+
+
 
 }
