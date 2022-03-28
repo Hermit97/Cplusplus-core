b59efa49c86517b474b33c3dd7726be425c48292
ShawnAli97 <shawn10ali@gmail.com>
2022-03-28 18:22:57 -0400

    num4 complete

--- ch4/Exercises/num4.cpp
+++ ch4/Exercises/num4.cpp
@@ -16,14 +16,52 @@ we look at a much more sophisticated simple calculator.*/
 int main(){
     double num1, num2;
     char op;
+    double arith;
 
-    std::cout << "Enter a number followed by a operator followed by another number.";
+
+    std::cout << "Enter a number followed by a operator followed by another number.\n";
+
+    std::cout << "Enter the first number: ";
     std::cin >> num1;
 
-    //Check for invalid number input
+    //Check for the first invalid number input
     while(std::cin.fail()){
-
-
+        std::cout << " Not an int\n";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cout << "Enter another number\n";
+        std::cin >> num1;
     }
+
+    std::cout << "Enter the operator: ";
+    std::cin >> op;
+
+    while(op != '+' && op != '-' && op != '*' && op != '/'){
+        std::cout << "Wrong input, please enter a relevant operator\n";
+        std::cin >> op;
+    }
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+    //Check for the first invalid number input
+    while(std::cin.fail()){
+        std::cout << " Not an int\n";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cout << "Enter another number\n";
+        std::cin >> num2;
+    }
+
+    if(op == '+')
+        arith = num1 + num2;
+    else if(op == '-')
+        arith = num1 - num2;
+    else if(op == '*')
+        arith = num1 * num2;
+    else if(op == '/')
+        arith = num1 / num2;
+
+    std::cout << "You entered: " << num1 << " " << op << " " << num2 <<  " = " << arith << "\n";
+
 
 }
