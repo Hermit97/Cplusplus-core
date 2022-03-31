f60d582ebe497b0b67d5344fb3c155edf746145d
ShawnAli97 <shawn10ali@gmail.com>
2022-03-31 18:53:27 -0400

    Add spelled out digit functionality

--- ch4/Exercises/num6.cpp
+++ ch4/Exercises/num6.cpp
@@ -6,4 +6,81 @@
 #include <cstdlib>
 #include <limits>
 
-/**/
+/*Modify the “mini calculator” from exercise 5 to accept (just) single-digit
+  numbers written as either digits or spelled out.*/
+
+int main(){
+    double num1, num2;
+    char op;
+    double arith;
+    bool isTrue = true;
+
+    std::vector<std::string> numValues = {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+    };
+
+
+    std::cout << "Enter a number followed by a operator followed by another number.\n";
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    while(num1 > 9 || num1 < -9){
+        std::cout << "Must be single digit, try again\n";
+        std::cin >> num1;
+    }
+
+    //Check for the first invalid number input
+    while(std::cin.fail()){
+        std::cout << " Not an int\n";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cout << "Enter another number\n";
+        std::cin >> num1;
+    }
+
+    std::cout << "Enter the operator: ";
+    std::cin >> op;
+
+    while(op != '+' && op != '-' && op != '*' && op != '/'){
+        std::cout << "Wrong input, please enter a relevant operator\n";
+        std::cin >> op;
+    }
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+    while(num2 > 9 || num2 < -9){
+        std::cout << "Must be single digit, try again\n";
+        std::cin >> num2;
+    }
+    //Check for the first invalid number input
+    while(std::cin.fail()){
+        std::cout << " Not an int\n";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cout << "Enter another number\n";
+        std::cin >> num2;
+    }
+
+    if(op == '+')
+        arith = num1 + num2;
+    else if(op == '-')
+        arith = num1 - num2;
+    else if(op == '*')
+        arith = num1 * num2;
+    else if(op == '/')
+        arith = num1 / num2;
+
+    std::cout << "You entered: " << num1 << " " << op << " " << num2 <<  " = " << arith << "\n";
+
+
+}
--- ch4/Exercises/test.cpp
+++ ch4/Exercises/test.cpp
@@ -4,8 +4,12 @@
 using namespace std;
 
 int main() {
-  int lower = 6;
-  int upper = 12;
-  int mid = (upper + lower) / 2;
-  std::cout << mid << "\n";
+  int input;
+  cin >> input;
+  while(input > 9 || input < -9){
+    std::cout << "Must be single digit, try again\n";
+    std::cin >> input;
+  }
+
+  cout << "OKASAY";
 }
