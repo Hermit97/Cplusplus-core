6ab23704bd9b7de759c3adc012fe2b43fc119144
ShawnAli97 <shawn10ali@gmail.com>
2022-03-31 13:56:45 -0400

    Finshed num 6

--- ch4/Exercises/num5.cpp
+++ ch4/Exercises/num5.cpp
@@ -15,37 +15,31 @@ their digit form;
 e.g., the input seven gives the output 7.*/
 
 int main(){
-int input;
-int i;
-std::vector<std::string> numValues = {
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-};
+    int input;
+    int i;
+    std::vector<std::string> numValues = {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+    };
 
-//ConvertIntToString
-std::cout << "Enter an integer to get its string value\n";
-std::cin >> input;
+    //ConvertIntToString
+    std::cout << "Enter an integer to get its string value\n";
+    std::cin >> input;
 
-if(input >= 0 && input <= 9){
-    std::cout << numValues[input] << "\n";
-}else{
-    std::cout << "Out of bounds\n";
-}
+    if(input >= 0 && input <= 9){
+        std::cout << numValues[input] << "\n";
+    }else{
+        std::cout << "Out of bounds\n";
+    }
 
-//ConvertStringToInt
-
-for(i = 0; i < numValues.size(); i++){
-    std::cout << numValues[input] <<  " is " << i << "\n";
-}
-
-
-
+    //ConvertStringToInt
+    std::cout << input << " as a string is " << numValues[input] << "\n";
 }
