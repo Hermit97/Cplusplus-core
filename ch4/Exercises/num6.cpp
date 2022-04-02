653cd893cbb63b395ef9b6678edb03e917394f1f
ShawnAli97 <shawn10ali@gmail.com>
2022-04-01 22:41:17 -0400

    not working

--- ch4/Exercises/num6.cpp
+++ ch4/Exercises/num6.cpp
@@ -12,7 +12,7 @@
 int main(){
     std::string num1, num2;
     char op;
-    double arith;
+    std::string arith;
     bool isTrue = true;
 
     std::vector<std::string> numValues = {
@@ -30,22 +30,15 @@ int main(){
     //Get string input as well as digits for single digit only
 
     std::cout << "Enter a number followed by a operator followed by another number.\n";
-
     std::cout << "Enter the first number: ";
     std::cin >> num1;
 
-    /*
-    while(num1 > 9 || num1 < -9){
-        std::cout << "Must be single digit, try again\n";
-        std::cin >> num1;
-    }
-    */
-
     //Does this take string input and int?? And are both limited to single digit??
     while(isTrue){
+
         for(int i = 0; i < numValues.size(); i++){
             //Check if the input is equal to either the index numbers or the actaul data in the vector
-            if (num1 == numValues[i] || num1 == numValues[]) {
+            if (num1 == numValues[i]) {
                 isTrue = false;
             }else{
                 std::cout << "The input is not single digit\n";
@@ -75,10 +68,13 @@ int main(){
 
     std::cout << "Enter the second number: ";
     std::cin >> num2;
+    /*
     while(num2 > 9 || num2 < -9){
         std::cout << "Must be single digit, try again\n";
         std::cin >> num2;
     }
+    */
+
     //Check for the first invalid number input
     while(std::cin.fail()){
         std::cout << " Not an int\n";
@@ -98,6 +94,4 @@ int main(){
         arith = num1 / num2;
 
     std::cout << "You entered: " << num1 << " " << op << " " << num2 <<  " = " << arith << "\n";
-
-
 }
