dd4bd0ffe8ceef94a9df8815579da2c11160ead7
ShawnAli97 <shawn10ali@gmail.com>
2022-04-03 02:35:27 -0400

    Finished num6

--- ch4/Exercises/num6.cpp
+++ ch4/Exercises/num6.cpp
@@ -7,9 +7,6 @@
 #include <cstdlib>
 #include <limits>
 
-/*Modify the “mini calculator” from exercise 5 to accept (just) single-digit
-  numbers written as either digits or spelled out.*/
-
 //Creates and gives vector values
 std::vector<std::string> numValues = {
         "zero",
@@ -24,31 +21,41 @@ std::vector<std::string> numValues = {
         "nine",
 };
 
-int number(){
-    int num = numValues.size();
-    bool isTrue = true;
-    std::cout << "Enter value for num1\n";
-    std::cin >> num;
+int number() {
+  int num1 = numValues.size();
+  std::string s;
 
-    //Check if number is out of bounds
-    while(isTrue){
-        if(num < 0 || num > 9){
-            std::cout << "Out of bounds, try again\n";
-            std::cin.clear();
-            std::cin >> num;
-        }else
-            isTrue = false;
+  if(std::cin >> num1){
+    std::cout << "You entered " << num1 << "\n";
+  }else{
+    std::cin.clear();
+    std::cin >> s;
+
+    for(int i = 0; i < numValues.size(); i++){
+      if(s == numValues[i])
+        num1 = i;
     }
-
-    std::string wordNum;
-    std::cin >> wordNum;
-
-    return num;
-
+    //std::cout << num1 << "\n";
+  }
+  return num1;
 }
 
-int main(){
+int main() {
+  int val1, val2;
+  char op;
 
-    number();
+  val1 = number();
+
+  std::cin >> op;
+  while(op != '+' && op != '=' && op != '*' && op != '/'){
+    std::cout << "Invalid ops\n";
+    std::cin.clear();
+    std::cin >> op;
+  }
+
+  val2 = number();
+
+  if(op == '+')
+    std::cout << val1 + val2 << "\n";
 
 }
