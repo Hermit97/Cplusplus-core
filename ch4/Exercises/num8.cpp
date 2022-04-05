9be00b344ce5211f167e5930e198302ec15d8490
ShawnAli97 <shawn10ali@gmail.com>
2022-04-04 23:40:47 -0400

    Checking for which square as 64 grains

---
+++ ch4/Exercises/num8.cpp
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <system_error>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+
+/*Try to calculate the number of rice grains that the inventor asked for in
+exercise 8 above. You’ll find that the number is so large that it won’t fit
+in an int or a double . Observe what happens when the number gets too
+large to represent exactly as an int and as a double . What is the larg-
+est number of squares for which you can calculate the exact number of
+grains (using an int )? What is the largest number of squares for which
+you can calculate the approximate number of grains (using a double )?*/
+
+int main() {
+
+    int squares;
+    int riceGrain = 1;
+
+    std::cin >> squares;
+
+    for(int i = 1; i <= squares; i++){
+        std::cout << "Square = " << squares << " Rice grain = " << riceGrain << "\n";
+        riceGrain = riceGrain * 2;
+    }
+
+}
--- ch4/Exercises/test.cpp
+++ ch4/Exercises/test.cpp
@@ -7,55 +7,14 @@
 #include <cstdlib>
 #include <limits>
 
-//Creates and gives vector values
-std::vector<std::string> numValues = {
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-};
+/*Find which square has 64 grains of rice, grain of rice doubles each square*/
 
-int number() {
-  int num1 = numValues.size();
-  std::string s;
+int main(){
+  int grainsOfRice = 64;
+  int squares = 1;
+  int oneGrainOfRice = ///mannn write it out
 
-  if(std::cin >> num1){
-    std::cout << "You entered " << num1 << "\n";
-  }else{
-    std::cin.clear();
-    std::cin >> s;
 
-    for(int i = 0; i < numValues.size(); i++){
-      if(s == numValues[i])
-        num1 = i;
-    }
-    //std::cout << num1 << "\n";
-  }
-  return num1;
-}
 
-int main() {
-  int val1, val2;
-  char op;
-
-  val1 = number();
-
-  std::cin >> op;
-  while(op != '+' && op != '=' && op != '*' && op != '/'){
-    std::cout << "Invalid ops\n";
-    std::cin.clear();
-    std::cin >> op;
-  }
-
-  val2 = number();
-
-  if(op == '+')
-    std::cout << val1 + val2 << "\n";
 
 }
