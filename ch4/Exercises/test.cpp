73d80e988e6db383d24d3ac57d29f06c13b087d9
ShawnAli97 <shawn10ali@gmail.com>
2022-04-09 04:02:11 -0400

    complete

--- ch4/Exercises/test.cpp
+++ ch4/Exercises/test.cpp
@@ -10,27 +10,32 @@
 
 
 std::vector<int> primes;
-std::vector<int> numbers;
-
-void isPrime(){
-    int primeNums = 3;
-    for(int i = 2; i <= 100; i++){
-        numbers.push_back(i);
-    }
-
-    //If numbers vector elements are prime, push them into primes vector.
-    for(int i = 2; i <= primeNums; i++){
-      if (primeNums % i == 0) {
-          std::cout << "Not a prime number " << i << "\n";
-      }else {
-          std::cout << "IS prime\n";
-      }
-    }
-}
 
 int main(){
-    isPrime();
+  int primeNum = 100;
+  bool isPrime = true;
 
+  for(int checkNum = 2; checkNum <= primeNum; checkNum++){
+    isPrime = true;
+
+    for(int factor = 2; factor <= checkNum / 2; factor++){
+      if(checkNum % factor == 0){
+        isPrime = false;
+      }
+
+
+    }
+    //primes.push_back(checkNum);
+    if(isPrime)
+      primes.push_back(checkNum);
+
+  }
+
+
+
+  for(int i = 0; i < primes.size(); i++){
+    std::cout << primes[i] << "\n";
+  }
 
 
 }
