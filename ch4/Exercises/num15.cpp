17704a4ae073f034daf7e0ecaa9cbecbda4415ed
ShawnAli97 <shawn10ali@gmail.com>
2022-04-11 23:17:38 -0400

    working on num15 custom sorting function

---
+++ ch4/Exercises/num15.cpp
@@ -0,0 +1,26 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <system_error>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+
+/*In the drill, you wrote a program that, given a series of numbers, found
+the max and min of that series. The number that appears the most times
+in a sequence is called the mode. Create a program that finds the mode of
+a set of positive integers.*/
+
+int main(){
+    int max, min;
+
+    //Enter and print series of numbers
+    for(int num; std::cin >> num;){
+      std::cout << "You entered " << num << "\n";
+    }
+
+    //Sort the numbers from lowest to greatest without sort()
+    for(int i = 0; i < num;)
+}
