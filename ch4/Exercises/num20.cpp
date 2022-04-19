70152292948fdb9a6cd9f16d994d952d731668ec
ShawnAli97 <shawn10ali@gmail.com>
2022-04-19 02:39:02 -0400

    Started num 20

---
+++ ch4/Exercises/num20.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <sys/types.h>
+#include <system_error>
+#include <vector>
+
+/*Modify the program from exercise 19 so that when you enter an integer,
+the program will output all the names with that score or score not found .*/
+
+int main(){
+    std::vector<std::string> names;
+    std::vector<int> scores;
+    std::string enterName;
+    bool isTrue = true;
+    int i, j;
+
+    while(isTrue){
+        std::string name;
+        int score;
+
+        std::cin >> name;
+        std::cin >> score;
+
+        //Check for same entries for names vector
+        for(int i = 0; i < names.size() && i < scores.size(); i++){
+            if(name == names[i] && score == scores[i]){
+              std::cout << "This already exists. Try again.\n";
+            }
+        }
+
+        if(name == "NoName" && score == 0){
+            isTrue = false;;
+            break;
+        }
+
+        names.push_back(name);
+        scores.push_back(score);
+    }
+
+    std::cout << "Result:\n";
+    for(int  i = 0; i < names.size() && i < scores.size(); i++){
+        std::cout << names[i];
+        std::cout << " " << scores[i] << "\n";
+    }
+
+    std::cout << "\n\n";
+
+    std::cout << "Enter a name to get the correspoinding score\n";
+    std::cin >> enterName;
+
+    //Maybe use a inner loop to check each of the names.
+    for(int i = 0; i < names.size(); i++){
+      if(enterName == names[i]){
+        std::cout << scores[i] << "\n";
+      }else{
+          std::cout << "Name not found\n";
+      }
+    }
+
+/*Modify the program from exercise 19 so that when you enter an integer,
+the program will output all the names with that score or score not found .*/
+std::cout << "Enter an integer: \n";
+}
