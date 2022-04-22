9bdfbf2c18c0688d6e6c7d95d2257779a5c259c8
ShawnAli97 <shawn10ali@gmail.com>
2022-04-21 23:42:09 -0400

    ch5

---
+++ ch5/TryThis/num1.cpp
@@ -0,0 +1,16 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <system_error>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+
+int main(){
+    int s1 = area(7;
+    int s2 = area(7)
+    Int s3 = area(7);
+    int s4 = area('7);
+}
