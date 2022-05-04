09dd0d2873f13e744da0e2220fa3bac2b5f3e5a8
Shawn <shawn10ali@gmail.com>
2022-05-04 14:22:41 -0400

    Caught and fixed all errors

--- ch5/TryThis/num1.cpp
+++ ch5/TryThis/num1.cpp
@@ -9,29 +9,35 @@ void error(std::string errorMessage){
   std::cout << errorMessage;
 }
 
+int area(int length, int width){
+  if(length <= 0 || width <= 0) error("non-positive area() arguemnt");
+  return length * width;
+}
+
 int framed_area(int x, int y){
   constexpr int frame_width = 2;
   if (x - frame_width <= 0 || y - frame_width <= 0)
     error("non-positive area() arguements called  by framed_area()");
-  return area(x - frame_width, y - frame_width));
-}
-
-int area(int length, int width){
-  if(length <= 0 || width <= 0) error("non-positive area() arguemnt");
-  return length * width;
+  return area(x - frame_width, y - frame_width);
 }
 
 int f(int x, int y, int z){
   int area1 = area(x, y);
   if (area1 <= 0)
     error("non-positive area");
-  int area2 = framed_area(1, z);
+  int area2 = framed_area(10, z);
   int area3 = framed_area(y, z);
-  double ratio = double (area1) / area3;
+  //double ratio = double (area1) / area3;
+  int ratio = (area1) / area3;
+
+  std::cout << "Area1 is: " << area1 << "\n";
+  std::cout << "Area2 is: " << area2 << "\n";
+  std::cout << "Area3 is: " << area3 << "\n";
+  std::cout << "Ratio is: " << ratio << "\n";
   return ratio;
+  /*Errors caught here are, returning double with the method type is int. casting the result of area1 / area3 as a double. */
 }
 
 int main(){
-  error("Hello\n");
-
+    f(100, 10, 20);
 }
