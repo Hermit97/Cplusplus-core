ba7076cad9eedc3bf6913debbf34678904a39334
ShawnAli97 <shawn10ali@gmail.com>
2022-05-04 00:08:46 -0400

    Function call error

--- ch5/TryThis/num1.cpp
+++ ch5/TryThis/num1.cpp
@@ -1,16 +1,37 @@
-#include <cctype>
 #include <iostream>
-#include <string>
-#include <system_error>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <cstdlib>
-#include <limits>
+
+/*Test this program with a variety of values. Print out the values of area1 ,
+area2 , area3 , and ratio . Insert more tests until all errors are caught. How
+do you know that you caught all errors? This is not a trick question; in this
+particular example you can give a valid argument for having caught all errors.*/
+
+void error(std::string errorMessage){
+  std::cout << errorMessage;
+}
+
+int framed_area(int x, int y){
+  constexpr int frame_width = 2;
+  if (x - frame_width <= 0 || y - frame_width <= 0)
+    error("non-positive area() arguements called  by framed_area()");
+  return area(x - frame_width, y - frame_width));
+}
+
+int area(int length, int width){
+  if(length <= 0 || width <= 0) error("non-positive area() arguemnt");
+  return length * width;
+}
+
+int f(int x, int y, int z){
+  int area1 = area(x, y);
+  if (area1 <= 0)
+    error("non-positive area");
+  int area2 = framed_area(1, z);
+  int area3 = framed_area(y, z);
+  double ratio = double (area1) / area3;
+  return ratio;
+}
 
 int main(){
-    int s1 = area(7;
-    int s2 = area(7)
-    Int s3 = area(7);
-    int s4 = area('7);
+  error("Hello\n");
+
 }
