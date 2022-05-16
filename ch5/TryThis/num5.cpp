6fe39303fac3e2e3b8cc073fed074656e71bddad
ShawnAli97 <shawn10ali@gmail.com>
2022-05-16 19:43:11 -0400

    Updating review

--- ch5/TryThis/num5.cpp
+++ ch5/TryThis/num5.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <exception>
 
-int main(){
-
+int main() {
+  try {
+    //<< your code here >> keep_window_open();
+    std::cout << "Success!" << "\n";
+    return 0;
+  } catch (std::exception &e) {
+   std::cerr << "error: " << e.what() << '\n';
+    return 1;
+  } catch (...) {
+    std::cerr << "Oops: unknown exception!\n";
+    return 2;
+  }
 }
