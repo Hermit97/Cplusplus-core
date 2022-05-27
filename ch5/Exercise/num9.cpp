0d84e8045f82ac497ed2789069e2163e7620d088
ShawnAli97 <shawn10ali@gmail.com>
2022-05-27 19:28:45 -0400

    finished num9

--- ch5/Exercise/num9.cpp
+++ ch5/Exercise/num9.cpp
@@ -1,4 +1,3 @@
-#include <cctype>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
@@ -7,64 +6,52 @@
 /*Modify the program from exercise 8 to write out an error if the result
 cannot be represented as an int .*/
 
-// CREATE A FUNCTION FOR GETTING INPUT FOR VECTOR. INSIDE OF THE FUNCTION CHEK
-// FOR NON INT INPUT
+const std::string intError = "Not an int";
+const std::string rangeError = "Out of bounds";
 
-const std::string intErrorMsg = "Entered input is not an int";
-const std::string rangError = "Out of range";
-const std::vector<int> nums;
-
-// Check amount of integers
-void checkAmount(int n, std::vector<int> nums) {
-  if (nums.size() > n)
-    throw std::runtime_error(rangError);
+//Check amount of integers
+void checkAmount(int n, std::vector<int> nums){
+      if(nums.size() > n)
+          throw std::runtime_error(rangeError);
 }
 
-// Check for int inputs
-void fillVector(std::vector<int> nums) {
-  for (int num; std::cin >> num;)
-    nums.push_back(num);
-   // if (!std::cin)
-     // throw std::runtime_error(intErrorMsg);
+int main(){
+    try{
+        std::vector<int> nums;
+        int sum = 0;
+        int howMany;
+        int vectorSize;
 
-}
+        std::cout << "How many numbers\n";
+        std::cin >> howMany;
+        if(!std::cin)
+          throw std::runtime_error(intError);
 
-// get sum
-int getSum(std::vector<int> nums) {
-  int sum;
-  for (int i = 0; i < nums.size(); i++) {
-    sum = nums[i] + sum;
-  }
-  return sum;
-}
+        for(int num; std::cin >> num;)
+            nums.push_back(num);
 
-int main() {
-  try {
-    // std::vector<int> nums;
-    int sum;
-    int howMany;
+        if(!std::cin.eof()){
+          std::cin.clear();
+          char end = ' ';
+          std::cin >> end;
+          if(end != '|')
+            throw std::runtime_error(intError);
+        }
 
-    std::cout << "How many numbers\n";
-    std::cin >> howMany;
-    if (!std::cin)
-      throw std::runtime_error(intErrorMsg);
 
-    // get input for vector
-    fillVector(nums);
+        //Check amount of inputs
+        checkAmount(howMany, nums);
 
-    // Check amount of inputs
-    checkAmount(howMany, nums);
+        //sum
+        for(int i = 0; i < nums.size(); i++){
+            sum = nums[i] + sum;
+        }
 
-    // sum
-    sum = getSum(nums);
-
-    // print sum
-    std::cout << "Sum is " << sum << "\n";
-    std::cout << "The size of the vector is " << nums.size() << "\n";
-    //} catch (std::vector<int> nums) {
-  } catch (std::exception &e) {
-    std::cerr << "Error: " << e.what() << "\n";
-  } catch (...) {
-    std::cerr << "Unexpected error";
-  }
+        //print sum
+        std::cout << "Sum is " << sum << "\n";
+        std::cout << "The size of the vector is " << nums.size() << "\n";
+    }catch(std::exception &e){
+        //std::cout << "Out of range\n";
+        std::cerr << "Error " << e.what() << "\n";
+    }
 }
