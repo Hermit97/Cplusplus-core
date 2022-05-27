d872db32fea3e2b15c9b3a4b58baa5fa1c4092b9
ShawnAli97 <shawn10ali@gmail.com>
2022-05-26 23:16:58 -0400

    Update num9.cpp

--- ch5/Exercise/num9.cpp
+++ ch5/Exercise/num9.cpp
@@ -7,6 +7,9 @@
 /*Modify the program from exercise 8 to write out an error if the result
 cannot be represented as an int .*/
 
+// CREATE A FUNCTION FOR GETTING INPUT FOR VECTOR. INSIDE OF THE FUNCTION CHEK
+// FOR NON INT INPUT
+
 const std::string intErrorMsg = "Entered input is not an int";
 const std::string rangError = "Out of range";
 const std::vector<int> nums;
@@ -18,47 +21,50 @@ void checkAmount(int n, std::vector<int> nums) {
 }
 
 // Check for int inputs
-void checkInt(int x) {
-  if (!std::cin >> x)
-    throw std::runtime_error(intErrorMsg);
+void fillVector(std::vector<int> nums) {
+  for (int num; std::cin >> num;)
+    nums.push_back(num);
+   // if (!std::cin)
+     // throw std::runtime_error(intErrorMsg);
+
+}
+
+// get sum
+int getSum(std::vector<int> nums) {
+  int sum;
+  for (int i = 0; i < nums.size(); i++) {
+    sum = nums[i] + sum;
+  }
+  return sum;
 }
 
 int main() {
   try {
-    std::vector<int> nums;
-    int sum = 0;
+    // std::vector<int> nums;
+    int sum;
     int howMany;
-    int vectorSize;
 
     std::cout << "How many numbers\n";
     std::cin >> howMany;
+    if (!std::cin)
+      throw std::runtime_error(intErrorMsg);
 
-    for (int num; std::cin >> num;){
-      //checkInt(num);
-      nums.push_back(num);
-      checkInt(num);
-    }
-
-    //Check if its an int
-    //checkInt(num);
+    // get input for vector
+    fillVector(nums);
 
     // Check amount of inputs
     checkAmount(howMany, nums);
 
     // sum
-    for (int i = 0; i < nums.size(); i++) {
-      sum = nums[i] + sum;
-    }
+    sum = getSum(nums);
 
     // print sum
     std::cout << "Sum is " << sum << "\n";
     std::cout << "The size of the vector is " << nums.size() << "\n";
-  //} catch (std::vector<int> nums) {
-  } catch (std::exception& e) {
+    //} catch (std::vector<int> nums) {
+  } catch (std::exception &e) {
     std::cerr << "Error: " << e.what() << "\n";
-    return 1;
-  }catch(...){
+  } catch (...) {
     std::cerr << "Unexpected error";
-    return 2;
   }
 }
