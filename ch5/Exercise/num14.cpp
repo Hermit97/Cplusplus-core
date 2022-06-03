fe6d69d9bff573d8c08e6833e8bd05292c398b09
Shawn <shawn10ali@gmail.com>
2022-06-03 03:41:28 -0400

    update add num14

---
+++ ch5/Exercise/num14.cpp
@@ -0,0 +1,13 @@
+/*Read (day-of-the-week,value) pairs from standard input. For example:
+Tuesday 23 Friday 56 Tuesday –3 Thursday 99
+Collect all the values for each day of the week in a vector<int> . Write out
+the values of the seven day-of-the-week vector s. Print out the sum of the
+values in each vector . Ignore illegal days of the week, such as Funday ,
+but accept common synonyms*/
+
+#include <iostream>
+
+int main(){
+
+
+}
