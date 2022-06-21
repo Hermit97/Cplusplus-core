40a347a0f58e7088ccc01256f10720627c08c671
ShawnAli97 <shawn10ali@gmail.com>
2022-06-21 19:09:33 -0400

    Update num4

--- ch6/Excercises/num4.cpp
+++ ch6/Excercises/num4.cpp
@@ -1,16 +1,63 @@
 /*Define a class Name_value that holds a string and a value. Rework exer-
 cise 19 in Chapter 4 to use a vector<Name_value> instead of two vector s.*/
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
-class Name_value{
-    public:
-        std::string names;
-        int scores;
-        Name_value(std::string n, int s)
-            : names(n), scores(s){} //Read more on page 183
-
-        //And read this scroll down almost to the end
-        /**https://cplusplus.com/doc/tutorial/classes/*/
-
+class Name_value {
+public:
+  std::string names;
+  int scores;
+  Name_value(std::string n, int s) : names(n), scores(s) {}
 };
+
+int main() {
+  std::vector<Name_value> info; // Intilized object called info from Name_value
+
+  std::string name;
+  int score;
+  std::string enterName;
+  bool isTrue = true;
+  bool duplicate = false;
+  bool nameFound = false;
+
+  while (isTrue) {
+    std::cin >> name;
+    std::cin >> score;
+
+    if (name == "Noname" && score == 0)
+      break;
+
+    for (Name_value n : info) {
+      if (n.names == name || n.scores == score) {
+        std::cout << "This already exists. Try again \n";
+        duplicate = true;
+      }
+    }
+    //Somethign wrong with stuff being pushed possibly the duplicate flase thingy
+
+    if (duplicate == false) {
+      info.push_back(Name_value(name, score));
+    }
+  }
+
+  std::cout << "Results: "
+            << " ";
+  for (Name_value x : info)
+    std::cout << x.names << " " << x.scores << "\n";
+
+  // Search for a name. It should print he requested name and score.
+  std::cout << "Enter a name\n";
+  std::cin >> enterName;
+  for (Name_value x : info) {
+    if (x.names == enterName) {
+        nameFound = true;
+        std::cout << x.names; //or just print enterName?
+        break;
+    }else
+        nameFound = false;
+  }
+  if (nameFound == false)
+      std::cout << "Name not found\n";
+}
