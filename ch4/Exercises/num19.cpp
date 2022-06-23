d46966866def52a770d93efe5ba4b611fdd2b86a
ShawnAli97 <shawn10ali@gmail.com>
2022-06-23 04:38:47 -0400

    Update num6

--- ch4/Exercises/num19.cpp
+++ ch4/Exercises/num19.cpp
@@ -45,7 +45,7 @@ int main() {
     }
 
     if(repeat){
-       repeat = true;
+       repeat = false;
     }else{
         names.push_back(name);
         scores.push_back(score);
--- ch6/Excercises/num5.cpp
+++ ch6/Excercises/num5.cpp
@@ -1,2 +1,38 @@
-/*Add the article the to the “English” grammar in §6.4.1, so that it can
-describe sentences such as “The birds fly but the fish swim.”*/
+#include <iostream>
+#include <vector>
+
+class string_stream {
+public:
+  std::string get();
+  void putBack(std::string);
+
+public:
+  bool full{false};
+  std::string buffer = {'0'};
+};
+
+void error(std::string er) { std::cout << er << "\n"; }
+
+void string_stream::putBack(std::string s) {
+  if (full)
+    error("putback() into a full buffer");
+  buffer = s;
+}
+
+std::string string_stream::get() {
+  if (full) {
+    full = false;
+    return buffer;
+  }
+
+  std::string s;
+  std::cin >> s;
+
+  return s;
+}
+
+string_stream stream;
+
+std::string noun(){
+    std::vector<std::string> noun;
+}
