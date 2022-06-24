a3d8b3a5accbb64ddd884d18b665c26c2090db40
ShawnAli97 <shawn10ali@gmail.com>
2022-06-23 20:03:46 -0400

    Update parser

--- ch6/Excercises/num5.cpp
+++ ch6/Excercises/num5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 
 class string_stream {
@@ -13,26 +14,35 @@ public:
 
 void error(std::string er) { std::cout << er << "\n"; }
 
-void string_stream::putBack(std::string s) {
-  if (full)
-    error("putback() into a full buffer");
-  buffer = s;
+void string_stream::putBack(std::string s){
+    if(full) error("putback() into a full buffer");
+    buffer = s;
+    full = true;
 }
 
-std::string string_stream::get() {
-  if (full) {
-    full = false;
-    return buffer;
-  }
-
-  std::string s;
-  std::cin >> s;
-
-  return s;
+std::string string_stream::get(){
+    if(full){
+        full = false;
+        return buffer;
+    }
+    std::string s;
+    std::cin >> s;
 }
 
 string_stream stream;
 
-std::string noun(){
-    std::vector<std::string> noun;
+bool noun();
+bool conjunction();
+bool verb();
+bool sentence();
+
+bool noun(){
+    std::vector<std::string> noun = {"birds", "fish", "C++"};
+    bool found = false;
+
+    std::string n = stream.string_stream::get();
+    for(std::string x : noun){
+        if(x == n)
+            found = true;
+    }
 }
