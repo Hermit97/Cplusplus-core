43a8cf631739b3fc28201c930d9f2020dbb9d6f6
Shawn <shawn10ali@gmail.com>
2022-10-12 13:46:25 -0400

    Update

---
+++ ch8/excercises/num2/printVectors.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+/*Write a function print() that prints a vector of int s to cout . Give it two
+arguments: a string for “labeling” the output and a vector .
+*/
+
+
+void print(std::string label, std::string output){
+	std::vector<int> bunchOfNums; 
+	int s;
+
+	std::cout << "Enter a number\n";
+	std::cin >> s;
+
+	for(int i = 0; i < bunchOfNums.size(); i++){
+
+	}
+
+
+}
+
+int main(){
+
+
+}
