842975fde75ed3ba9f7f657594aca7bbb37df85b
ShawnAli97 <shawn10ali@gmail.com>
2022-10-13 15:23:28 -0400

    Still needs work on the error management

--- ch8/excercises/num2/printVectors.cpp
+++ ch8/excercises/num2/printVectors.cpp
@@ -5,27 +5,48 @@
 arguments: a string for “labeling” the output and a vector .
 */
 
-
-void print(std::string label, std::string output){
-	std::vector<int> bunchOfNums; 
+void print(std::string label, std::vector<int> bunchOfNums){
 	int s;
+	char quit = 'q';
+	int change = static_cast<int>(quit);
+	
+	while(std::cin){
+		std::cout << "Enter a number\n";
+		std::cin >> s;
 
-	std::cout << "Enter a number\n";
-	std::cin >> s;
+		if(std::cin.peek() == 'q')
+			std::cout << "quit";
+		bunchOfNums.push_back(s);
 
-	for(int i = 0; i < bunchOfNums.size(); i++){
-		bunchOfNums.push_back(i);
+		/*while(!std::cin){
+			if(std::cin.peek() == 'q')
+				std::cout << "quit";
+
+			bunchOfNums.pop_back();
+			break;
+		}*/
+
+		
+		if(s != int('q')){
+			//if(std::cin.peek() == 'q')
+			//	std::cout << "quit";
+
+			bunchOfNums.pop_back();
+			break;
+		}else
+			std::cout << "ERROR\n";
+
 	}
 
-	for(int i = 0; i < bunchOfNums; i++){
-		std::cout bunchOfNums[i] << "\n";
+	for(int i = 0; i < bunchOfNums.size(); i++){
+		std::cout << label << ":" << bunchOfNums[i] << "\n";
 	}
 
 }
 
 int main(){
-	std::string l, o;
-
-	print(
-
+	std::vector<int> bunchOfNums; 
+	std::string l;
+	l = "Numbers";
+	print(l, bunchOfNums);
 }
