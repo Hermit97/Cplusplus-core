2cd8c27d53e81435cd154ba649943114155fc201
Shawn <shawn10ali@gmail.com>
2022-10-14 02:33:22 -0400

    Breaks after q input but buggy vector values

--- ch8/excercises/num2/printVectors.cpp
+++ ch8/excercises/num2/printVectors.cpp
@@ -7,41 +7,23 @@ arguments: a string for “labeling” the output and a vector .
 
 void print(std::string label, std::vector<int> bunchOfNums){
 	int s;
-	char quit = 'q';
-	int change = static_cast<int>(quit);
-	
+	std::string quit;
 	while(std::cin){
-		std::cout << "Enter a number\n";
-		std::cin >> s;
-
-		if(std::cin.peek() == 'q')
-			std::cout << "quit";
-		bunchOfNums.push_back(s);
-
-		/*while(!std::cin){
-			if(std::cin.peek() == 'q')
-				std::cout << "quit";
-
-			bunchOfNums.pop_back();
+		std::cout << "Enter a number or q to quit\n";
+		std::cin >> quit;
+		if(quit == "q"){
+			//bunchOfNums.pop_back();
 			break;
-		}*/
+		}else {
+			std::cin >> s;
+			bunchOfNums.push_back(s);
+		}
 
-		
-		if(s != int('q')){
-			//if(std::cin.peek() == 'q')
-			//	std::cout << "quit";
-
-			bunchOfNums.pop_back();
-			break;
-		}else
-			std::cout << "ERROR\n";
-
-	}
+		}
 
 	for(int i = 0; i < bunchOfNums.size(); i++){
 		std::cout << label << ":" << bunchOfNums[i] << "\n";
 	}
-
 }
 
 int main(){
