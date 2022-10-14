ba166ce09030f468efe4525f810b9424d77d23a1
Shawn <shawn10ali@gmail.com>
2022-10-14 16:48:32 -0400

    Finished num2

--- ch8/excercises/num2/printVectors.cpp
+++ ch8/excercises/num2/printVectors.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
 
 /*Write a function print() that prints a vector of int s to cout . Give it two
 arguments: a string for “labeling” the output and a vector .
 */
 
-void print(std::string label, std::vector<int> bunchOfNums){
-	int s;
-	std::string quit;
-	while(std::cin){
-		std::cout << "Enter a number or q to quit\n";
-		std::cin >> quit;
-		if(quit == "q"){
-			//bunchOfNums.pop_back();
-			break;
-		}else {
-			std::cin >> s;
-			bunchOfNums.push_back(s);
-		}
+void print(std::string label, std::vector<int> bunchOfNums) {
+  std::cout << "Enter a number or q to quit\n";
+  std::string quit;
+  while (std::cin) {
+    std::cin >> quit;
+    if (quit == "q")
+      break;
+    else {
+      int num = atoi(quit.c_str());
+      bunchOfNums.push_back(num);
+    }
+  }
 
-		}
-
-	for(int i = 0; i < bunchOfNums.size(); i++){
-		std::cout << label << ":" << bunchOfNums[i] << "\n";
-	}
+  for (int i = 0; i < bunchOfNums.size(); i++) {
+    std::cout << label << ":" << bunchOfNums[i] << "\n";
+  }
 }
 
-int main(){
-	std::vector<int> bunchOfNums; 
-	std::string l;
-	l = "Numbers";
-	print(l, bunchOfNums);
+int main() {
+  std::vector<int> bunchOfNums;
+  std::string l;
+  l = "Numbers";
+  print(l, bunchOfNums);
 }
