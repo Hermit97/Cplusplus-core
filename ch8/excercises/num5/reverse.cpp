c4ce04b0ad8eca3284c6ef313557930925141acb
ShawnAli97 <shawn10ali@gmail.com>
2022-10-19 23:03:06 -0400

    updae

---
+++ ch8/excercises/num5/reverse.cpp
@@ -0,0 +1,14 @@
+/*Write two functions that reverse the order of elements in a vector<int> .
+For example, 1, 3, 5, 7, 9 becomes 9, 7, 5, 3, 1. The first reverse function
+should produce a new vector with the reversed sequence, leaving its orig-
+inal vector unchanged. The other reverse function should reverse the
+elements of its vector without using any other vector s (hint: swap ).
+*/
+
+#include <iostream>
+#include <vector>
+
+void originalVector(){
+    std::vector<int> originalVector;
+    std::cout << "Enter values for the first vector\n";
+}
