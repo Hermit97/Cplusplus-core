7c7bc99d8a247e158221c657b2d1e77ddef5b998
ShawnAli97 <shawn10ali@gmail.com>
2022-10-22 16:50:15 -0400

    Function now werks but reverse logic iw wrong

--- ch8/excercises/num5/reverse.cpp
+++ ch8/excercises/num5/reverse.cpp
@@ -8,80 +8,80 @@ elements of its vector without using any other vector s (hint: swap ).
 #include <iostream>
 #include <vector>
 
-class ReverseVector{
-    public:
-        std::vector<int> originalVector();
-        //std::vector<int> reVector(std::vector<int>& v);
-        std::vector<int> reVector();
-        void swap(int x, int y);
+class ReverseVector {
+public:
+  // std::vector<int> originalVector();
+  void originalVector();
+  // std::vector<int> reVector(std::vector<int>& v);
+  void reVector();
+  // void reVector();
+  void swap(int x, int y);
+
+  std::vector<int> oldVector;
+  std::vector<int> newVector;
+  void printNewVector();
 };
 
-void ReverseVector::swap(int x, int y){
-    int temp = x;
-    x = y;
-    y = temp;
+void ReverseVector::swap(int x, int y) {
+  int temp = x;
+  x = y;
+  y = temp;
 }
 
-
-//This function is gettingv alled later on in a loop causing it to keep looping. FIX That
-
-std::vector<int> ReverseVector::originalVector(){
-    std::vector<int> originalV;
-    std::cout << "Enter values for the first vector\n";
-    std::string num;
-    std::string quit = "q";
-    int number;
-    bool start = true; 
-    while(start){
-        std::cout << "Enter a number for the vector\n"; 
-        std::cin >> num;
-        if(num == quit)
-            start = false;
-        //number = std::stoi(num);
-        number = atoi(num.c_str());
-        originalV.push_back(number);
+// This function is gettingv alled later on in a loop causing it to keep
+// looping. FIX That std::vector<int> ReverseVector::originalVector(){
+void ReverseVector::originalVector() {
+  // std::vector<int> originalV;
+  std::cout << "Enter values for the first vector\n";
+  std::string num;
+  std::string quit = "q";
+  int number;
+  bool start = true;
+  while (start == true) {
+    std::cout << "Enter a number for the vector\n";
+    std::cin >> num;
+    if (num == quit) {
+      start = false;
+      break;
     }
+    // number = std::stoi(num);
+    number = atoi(num.c_str());
+    // originalV.push_back(number);
+    oldVector.push_back(number);
+  }
 
-    std::cout << "This is the old vector\n";
-    for(int i = 0; i < originalV.size(); ++i){
-        std::cout << originalV[i] << "\n";
-    }
-    return originalV;
+  std::cout << "This is the old vector\n";
+  for (int i = 0; i < oldVector.size(); ++i) {
+    std::cout << oldVector[i] << "\n";
+  }
+  // return originalV;
 }
 
-std::vector<int> ReverseVector::reVector(){
-    std::vector<int> newVector;  
-    ReverseVector r1;
-    int vectorBigNum;
-    int vectorSmallNum;
-    
-    int vectorSize = r1.originalVector().size();
-    //Could try bubble sort to get the largest i first push it then the low one at the end.
-    for(int i = 0; i < vectorSize; ++i){
-        int x = i;
-        int y = i + 1;
-        swap(x, y);
-        newVector.push_back(i + 1);
+void ReverseVector::reVector() {
+  ReverseVector r1;
+
+  // int vectorSize = r1.originalVector().size();
+  // Could try bubble sort to get the largest i first push it then the low one
+  // at the end. for(int i = 0; i < vectorSize; ++i){
+  for (int i = 0; i < oldVector.size(); ++i) {
+    for (int j = i; j < oldVector.size(); ++j) {
+      if (i > j)
+        swap(i, j);
     }
-    return newVector;
+  newVector.push_back(i);
+  }
 }
 
-void printNewVector(){
-    ReverseVector r1;
-    std::vector <int> newV = r1.reVector();
-
-    std::cout << "This is the same vector reversed\n";
-    for(int i = 0; i < r1.reVector().size(); ++i){
-        std::cout << newV[i] << "\n";
-    }
+void ReverseVector::printNewVector() {
+  std::cout << "This is the same vector reversed\n";
+  for (int i = 0; i < newVector.size(); ++i) {
+    std::cout << newVector[i] << "\n";
+  }
 }
 
-int main(){
-    ReverseVector r1; 
-    r1.originalVector();
-    r1.reVector();
-    printNewVector();
-
-    
-    
+int main() {
+  ReverseVector r1;
+  r1.originalVector();
+  r1.reVector();
+  r1.printNewVector();
 }
