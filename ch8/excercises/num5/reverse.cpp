926171e7ecad81bb70a4193b3b75a9537243c6f7
ShawnAli97 <shawn10ali@gmail.com>
2022-10-21 20:30:09 -0400

    Implement a bubble sort for reversing

--- ch8/excercises/num5/reverse.cpp
+++ ch8/excercises/num5/reverse.cpp
@@ -8,7 +8,50 @@ elements of its vector without using any other vector s (hint: swap ).
 #include <iostream>
 #include <vector>
 
-void originalVector(){
+class ReverseVector{
+    public:
+        std::vector<int> originalVector();
+        std::vector<int> reVector(std::vector<int>& v);
+};
+
+std::vector<int> ReverseVector::originalVector(){
     std::vector<int> originalVector;
     std::cout << "Enter values for the first vector\n";
+    std::string num;
+    std::string quit = "q";
+    int number;
+    
+    while(num != quit){
+        std::cout << "Enter a number for the vector\n"; 
+        std::cin >> num;
+        if(num == quit)
+            break;
+        number = std::stoi(num);
+        originalVector.push_back(number);
+    }
+
+    for(int i = 0; i < originalVector.size(); ++i){
+        std::cout << originalVector[i] << "\n";
+    }
+
+    return originalVector;
+}
+
+std::vector<int> ReverseVector::reVector(std::vector<int> &v){
+    std::vector<int> newVector;  
+    ReverseVector r1;
+    int vectorBigNum;
+    int vectorSmallNum;
+    
+    //Could try bubble sort to get the largest i first push it then the low one at the end.
+    for(int i = 0; i < r1.originalVector().size(); ++i){
+        int vectorSize = r1.originalVector().size();
+        if(vectorSize 
+    }
+    return newVector;
+}
+
+int main(){
+    ReverseVector r1;
+    r1.originalVector();
 }
