7701185f3452c7f41d78fb63e7e7fc353226b996
ShawnAli97 <shawn10ali@gmail.com>
2022-10-23 20:33:11 -0400

    Update

---
+++ ch8/excercises/num6/reverseString.cpp
@@ -0,0 +1,8 @@
+/*Write versions of the functions from exercise 5, but with a vector<string> .*/
+
+#include <iostream>
+#include <vector>
+
+class ReverseString{
+    std::vector<std::string> originalString;
+};
