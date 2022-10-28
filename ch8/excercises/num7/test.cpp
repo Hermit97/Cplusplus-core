30de321588346c3dfa4d90d4c21b2933c339cf4c
Shawn <shawn10ali@gmail.com>
2022-10-28 16:44:38 -0400

    Test file

---
+++ ch8/excercises/num7/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*Get name and age. Then print them both side by side. Take 5 names/ages*/
+std::vector<std::string> names;
+std::vector<int> ages;
+
+// Get names
+void getNames(std::vector<std::string>& n){
+    std::string name;
+    std::cin >> name;
+}
