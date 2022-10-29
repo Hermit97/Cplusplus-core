6c0a4854b6170431c7298b71bbe748a8180e23e8
ShawnAli97 <shawn10ali@gmail.com>
2022-10-29 16:19:27 -0400

    Wont print printFirst wont print out the vectors

--- ch8/excercises/num7/test.cpp
+++ ch8/excercises/num7/test.cpp
@@ -1,13 +1,51 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 /*Get name and age. Then print them both side by side. Take 5 names/ages*/
 std::vector<std::string> names;
 std::vector<int> ages;
 
 // Get names
-void getNames(std::vector<std::string>& n){
-    std::string name;
-    std::cin >> name;
+std::string getNames() {
+  std::string name;
+  std::cin >> name;
+  return name;
+}
+
+int getAge() {
+  int age;
+  std::cin >> age;
+  return age;
+}
+
+void setUpName() {
+  for (int i = 0; i < 2; ++i) {
+    getNames();
+  }
+  std::cout << "Names end\n";
+}
+
+void setUpVectorAge() {
+  for (int i = 0; i < 2; ++i) {
+    getAge();
+  }
+  std::cout << "Ages end\n";
+}
+
+void setUp() {
+  setUpName();
+  setUpVectorAge();
+}
+
+void printFirst() {
+  //setUp();
+  std::cout << "TEST\n";
+  for (int i = 0; i < names.size() && ages.size(); ++i) {
+    std::cout << "Names: " << names[i] << " age: " << ages[i] << "\n";
+  }
+}
+int main() {
+  setUp();
+  printFirst();
 }
