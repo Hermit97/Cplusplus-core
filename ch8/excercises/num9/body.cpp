36ab4ca190a9ff8295101f4c90d0bbad295ea412
ShawnAli97 <shawn10ali@gmail.com>
2022-11-05 21:00:41 -0400

    Update

--- ch8/excercises/num9/body.cpp
+++ ch8/excercises/num9/body.cpp
@@ -1,7 +1,12 @@
+/*Write a function that given two vector<double> s price and weight com-
+putes a value (an “index”) that is the sum of all price[i]*weight[i] . Make
+sure to have weight.size()==price.size() .*/
+
 #include <iostream>
 #include <vector>
 
 std::vector<double> price;
+std::vector<double> weight;
 
 
 int main(){
