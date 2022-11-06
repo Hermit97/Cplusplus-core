cbe32aacf50254bb93f8437ce974b5a2ef2a2c7b
ShawnAli97 <shawn10ali@gmail.com>
2022-11-06 01:43:46 -0500

    Update

--- ch8/excercises/num9/body.cpp
+++ ch8/excercises/num9/body.cpp
@@ -2,13 +2,29 @@
 putes a value (an “index”) that is the sum of all price[i]*weight[i] . Make
 sure to have weight.size()==price.size() .*/
 
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
 std::vector<double> price;
 std::vector<double> weight;
 
+bool getPrice(){
+    std::string w;
+    double yourWeight;
+    bool isTrue = true;
+    while(isTrue){
+        std::cin >> yourWeight;
+        if(w == "q")
+            return isTrue = false;
+        yourWeight= std::atoi(w.c_str());
+        weight.push_back(yourWeight);
+    }
+}
 
 int main(){
-
+    getPrice();
+    for(double d : weight){
+        std::cout << d << "\n";
+    }
 }
