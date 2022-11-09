4367a90a7cf288b7197c90b29990234be5d4b13f
ShawnAli97 <shawn10ali@gmail.com>
2022-11-09 17:38:37 -0500

    Checks if both vectors are equal

--- ch8/excercises/num9/body.cpp
+++ ch8/excercises/num9/body.cpp
@@ -9,40 +9,71 @@ sure to have weight.size()==price.size() .*/
 #include <string>
 #include <vector>
 
-std::vector<double> price;
-std::vector<double> weight;
-
 const std::string wrongInput = "Invalid Input";
+const std::string notEqual = "Vectors are not equal";
 
-void error(std::string er) { std::cout << er; }
+void error(std::string er) { std::cout << er << "\n"; }
 
-void getPrice() {
+void getWeight(std::vector<double> &weight) {
   std::string w;
   double yourWeight;
   bool isTrue = true;
-  while (isTrue == true){
-    if(std::cin >> yourWeight){
+  while (isTrue == true) {
+    if (std::cin >> yourWeight) {
       weight.push_back(yourWeight);
-    }else{
+    } else {
       std::cin.clear();
       std::cin >> w;
-      if(w == "q")
+      if (w == "q")
         break;
-      else{
+      else {
         std::cout << wrongInput << "\n";
       }
     }
-
   }
 }
 
-  int main() {
-    try {
-      getPrice();
-      for (double d : weight)
-        std::cout << d << "\n";
-    } catch (std::exception &e) {
-      std::cerr << "Error: " << e.what() << "\n";
-      return 1;
+void getPrice(std::vector<double> &price) {
+  std::string w;
+  double yourPrice;
+  bool isTrue = true;
+  while (isTrue == true) {
+    if (std::cin >> yourPrice) {
+      price.push_back(yourPrice);
+    } else {
+      std::cin.clear();
+      std::cin >> w;
+      if (w == "q")
+        break;
+      else {
+        error(wrongInput);
+      }
     }
   }
+}
+
+void equalSize(std::vector<double> &w, std::vector<double> &p) {
+  if(w.size() != p.size())
+    throw std::runtime_error(notEqual);
+}
+
+int main() {
+  try {
+    std::vector<double> weight;
+    std::vector<double> price;
+    getWeight(weight);
+    std::cout << "Weights: \n";
+    getPrice(price);
+    std::cout << "Weights: \n";
+    for (double d : weight)
+      std::cout << d << "\n";
+    std::cout << "Prices: \n";
+    for (double p : price)
+      std::cout << p << "\n";
+
+    equalSize(weight,price);
+  } catch (std::exception &e) {
+    std::cerr << "Error: " << e.what() << "\n";
+    return 1;
+  }
+}
