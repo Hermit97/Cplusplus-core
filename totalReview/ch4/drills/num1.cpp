14fa3efc8a94f50e759eab9b86d6382a7378f950
ShawnAli97 <shawn10ali@gmail.com>
2023-07-13 05:23:17 -0400

    Update

---
+++ totalReview/ch4/drills/num1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int num1, num2;
+    while(std::cin >> num1 >> num2){
+        std::cout << num1 << " " << num2 << "\n";
+    }
+
+    return 0;
+
+}
