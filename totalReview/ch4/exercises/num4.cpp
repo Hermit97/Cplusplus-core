dca45e094c81f0610308337407ea6c916519e1ad
ShawnAli97 <shawn10ali@gmail.com>
2023-08-07 15:28:43 -0400

    finished

--- totalReview/ch4/exercises/num4.cpp
+++ totalReview/ch4/exercises/num4.cpp
@@ -7,3 +7,32 @@ program output should be The sum of 35.6 and 24.1 is 59.7. In Chapter 6
 we look at a much more sophisticated simple calculator.*/
 
 #include <iostream>
+
+int main(){
+    int num1, num2;
+    char op;
+
+    std::cout << "Enter a number opereator then a number\n";
+    std::cin >> num1 >> op >> num2;
+
+    if(op == '+'){
+        int sum = num1 + num2;
+        std::cout << sum << "\n";
+    }
+    else if(op == '-'){
+        int diff = num1 - num2;
+        std::cout << diff << "\n";
+    }
+    else if(op == '*'){
+        int product = num1 * num2;
+        std::cout << product << "\n";
+    }
+    else if(op == '/'){
+        int div = num1 / num2;
+        std::cout << div << "\n";
+    }
+
+
+
+
+}
