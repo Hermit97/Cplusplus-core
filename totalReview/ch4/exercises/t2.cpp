80185ee62a7f78bb4a2177661dc6a558d449978e
ShawnAli97 <shawn10ali@gmail.com>
2023-08-06 21:23:11 -0400

    Finds the correct number

--- totalReview/ch4/exercises/t2.cpp
+++ totalReview/ch4/exercises/t2.cpp
@@ -5,6 +5,33 @@ int main(){
     int high = 100;
     int questions = 7;
     int guess;
+    char user;
 
-    std::cout << "E";
+    std::cout << "I am going to guess the number between 1 and 100\n";
+    for(int i = 0; i < questions; ++i){
+        guess = (high + low) / 2;
+
+        std::cout << "If the number greater than " << guess <<  " then enter y otherwise enter n" << "\n";
+        std::cin >> user;
+
+        if(user == 'Y' || user == 'y'){
+            low = guess + 1;
+        }
+        else if(user == 'N' || user == 'n'){
+            high = guess;
+        }
+
+
+
+        if(high == low){
+          std::cout << "Ive found the correct number, it is " << high << " it took " << i << " attemnpts\n";
+          break;
+        }
+
+        if(low > high){
+            std::cout << "ERRRROR!\n";
+        }
+
+
+    }
 }
