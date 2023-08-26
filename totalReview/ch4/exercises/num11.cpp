4c729f938715697fce3f2fcfbe412891619bdf1d
ShawnAli97 <shawn10ali@gmail.com>
2023-08-26 19:54:01 -0400

    Update

--- totalReview/ch4/exercises/num11.cpp
+++ totalReview/ch4/exercises/num11.cpp
@@ -5,22 +5,31 @@ program using this method.*/
 #include <iostream>
 #include <vector>
 
-void sieve(int n){
-    std::vector<bool> a = (n + 1, true);
-    //a[0] = false;
+void sieve(int n) {
+  std::vector<bool> a (n + 1, true);
+  std::vector<int> primes;
+   a[0] = a[1] = false;
 
-    for(int i = 1; i <= n; ++i)
-	a.push_back(i);
+  //for (int i = 1; i <= n; ++i)
+    //a.push_back(i);
 
-    for(int i = 2; i * i < n; ++i){
-	if(a[i] == true){
-
-	}
+  for(int i = 2; i * i < n; ++i) {
+    if (a[i]) {
+        for(int j = i * i; j <= n; j += i){
+            a[j] = false;
+        }
     }
-}    
+  }
+  for(int i = 1; i < a.size(); ++i){
+      if(a[i]){
+          primes.push_back(i);
+      }
+  }
+  for(int x : primes)
+      std::cout << x << "\n";
+}
 
-int main(){
-    int n = 30;
-    sieve(n);
-    
+int main() {
+  int n = 100;
+  sieve(n);
 }
