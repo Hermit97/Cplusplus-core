0eaf98416990ccec662a79d54b75d03c1b88baa0
Shawn <shawn10ali@gmail.com>
2023-08-27 14:03:48 -0400

    update

--- totalReview/ch4/exercises/num14.cpp
+++ totalReview/ch4/exercises/num14.cpp
@@ -4,15 +4,31 @@ strings.*/
 #include <iostream>
 #include <vector>
 
+void bubble_sort(std::vector<int> num){
+    for(int i = 0; i < num.size(); ++i){
+	if(num[i] > num[i+1]){
+	    int temp = num[i];
+	    num[i] = num[i+1];
+	    num[i+1] = num[i];
+	}
+    }
+}
 int find_min(std::vector<int> num){
-
+    bubble_sort(num);
+    int min = num[0];
+    return min;
+    
 }
 
 int find_max(std::vector<int> num){
-
+    int end = num.size();
+    int max = num[end];
+    return max;
 }
 
 int find_mode(std::vector<int> num) {
+    int mode;
+    return mode;
 
 }
 
@@ -20,6 +36,7 @@ int main(){
     std::vector<int> numbers;
     int n;
     while(std::cin >> n)
+	numbers.push_back(n);
 
 
 
