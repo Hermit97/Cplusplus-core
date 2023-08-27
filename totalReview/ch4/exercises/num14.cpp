5ea07a17efb951b7f61ce9f78f6bf88924a90da9
ShawnAli97 <shawn10ali@gmail.com>
2023-08-27 12:36:24 -0400

    update

---
+++ totalReview/ch4/exercises/num14.cpp
@@ -0,0 +1,26 @@
+/*Write a program that finds the min, max, and mode of a sequence of
+strings.*/
+
+#include <iostream>
+#include <vector>
+
+int find_min(std::vector<int> num){
+
+}
+
+int find_max(std::vector<int> num){
+
+}
+
+int find_mode(std::vector<int> num) {
+
+}
+
+int main(){
+    std::vector<int> numbers;
+    int n;
+    while(std::cin >> n)
+
+
+
+}
