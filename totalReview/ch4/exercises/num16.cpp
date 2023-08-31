30e89883f6eac65bb397137c91a30d46766314ff
ShawnAli97 <shawn10ali@gmail.com>
2023-08-30 22:27:14 -0400

    update

--- totalReview/ch4/exercises/num16.cpp
+++ totalReview/ch4/exercises/num16.cpp
@@ -1,31 +1,36 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <math.h>
+#include <vector>
 /*Write a program to solve quadratic equations. A quadratic equation is of
 the form ax^2+bx+c=0 x = (-b ± √(b^2 - 4ac)) / 2a*/
 
 int main() {
-    int a, x, b, c;
-    int squared;
-    double pos_quadratic;
-    int sq;
-    double pos_final_quad;
-    int denom = 2 * a;
+  double a, b, c;
+  double squared;
+  double pos_quadratic;
+  double sq;
+  double pos_final_quad;
+  double neg_final_quad;
 
-    std::cout << "Enter a x b c\n";
-    std::cin >> a >> x >> b >> c;
+  std::cout << "Enter a x b c\n";
+  std::cin >> a >> b >> c;
+  double denom = 2.0 * a;
 
-    squared = b*b;
-    sq = squared - (4 * (a*c));
+  squared = b * b;
+  if (sq < 0)
+    std::cout << "No real solutions\n";
+  else {
 
-    pos_quadratic = -(b) + std::sqrt(sq);
+  sq = squared - (4 * a * c);
 
-    pos_final_quad = pos_quadratic / denom;
-    std::cout << pos_final_quad << "\n";
-    //std::cout << pos_quadratic << "\n";
+  pos_quadratic = std::sqrt(sq);
 
-
-
+  pos_final_quad = (-b + pos_quadratic) / (2.0 * a);
+  neg_final_quad = (-b - pos_quadratic) / (2.0 * a);
+  std::cout << "Answer for pos: " << pos_final_quad << "\n";
+  std::cout << "Answer for neg: " << neg_final_quad << "\n";
+  }
+  return 0;
 }
