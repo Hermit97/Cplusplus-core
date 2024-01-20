f8a6d27463b8a23a7c06083ea600ec5dac125a73
ShawnAli97 <shawn10ali@gmail.com>
2024-01-20 18:29:53 -0500

    f to c & c to f support added

---
+++ totalReview/ch5/exercise/f_and_c.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "error.h"
+
+//Fahrenheit f = 9/5 * c + 32
+std::string to_low_c = "Cannot be below absolute zero (-273.15C)";
+std::string to_low_f = "Cannot be below absolute zero (-459.67F)";
+
+double c_to_f(double c){
+  double half = (9.0/5);
+  double absolute_zero = -273.15;
+  double f = half * c + 32;
+  if(c < absolute_zero)
+    error(to_low_c);
+  return f;
+}
+
+//Celsius c = 5/9 * (F - 32)
+double f_to_c(double f){
+  double half = (9.0/5);
+  //double c = half * (f + 32);
+  double c = (f - 32) * 5;
+  double result = c / 9;
+  return result;
+}
+
+int main(){
+  double num;
+  std::cout << "Enter a number to be converted\n";
+  std::cin >> num;
+
+  try{
+  double fahrenheit = c_to_f(num);
+  std::cout << fahrenheit << "\n";
+
+  double celsius = f_to_c(fahrenheit);
+  std::cout << celsius << "\n";
+  }
+
+  catch(std::runtime_error& e){
+    std::cerr << "runtime error "  << e.what() << "\n";
+  }
+}
