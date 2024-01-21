adbd09da2389fe52cca9279be21f4c2b2cab166b
ShawnAli97 <shawn10ali@gmail.com>
2024-01-21 03:56:03 -0500

    Getting -nan when testing the sqrt in the numerator

--- totalReview/ch5/exercise/quadratic.cpp
+++ totalReview/ch5/exercise/quadratic.cpp
@@ -3,14 +3,19 @@
 #include <cmath>
 
 //positive
-q_pos(double b, double a, double c){
+double q_pos(double b, double a, double c){
   //put in a error check for b2 - 4ac if its less than zero make sure it fails
   double formula = -b + (sqrt((b*b) - 4 * (a * c)));
-
   double result = formula / (2 * a);
+  return result;
 }
 
 
 int main(){
+  double a, b, c;
+  std::cout << "Enter a b c\n";
+  std::cin >> b >> a >> c;
+  double quad = q_pos(b, a, c);
+  std::cout << quad << "\n";
 
 }
