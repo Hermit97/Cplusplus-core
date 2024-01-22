d910b86f8985981fd9662619453bca397244d99f
ShawnAli97 <shawn10ali@gmail.com>
2024-01-21 20:01:35 -0500

    positive quad support added needs negative

--- totalReview/ch5/exercise/quadratic.cpp
+++ totalReview/ch5/exercise/quadratic.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include "error.h"
 #include <cmath>
+#include <stdexcept>
 
 //positive
+std::string neg_root = "No real discriminants";
 double q_pos(double b, double a, double c){
   //put in a error check for b2 - 4ac if its less than zero make sure it fails
-  double formula = -b + (sqrt((b*b) - 4 * (a * c)));
-  double result = formula / (2 * a);
-  return result;
+  //double formula = -b + (sqrt((b*b) - 4 * (a * c)));
+  double squared = b * b;
+  double end = a * c;
+  double inside = squared - 4 * end;
+  if(inside < 0)
+    error(neg_root);
+  return inside;
 }
 
+double numerator(double b){
+  double opposite_coefficient = -b;
+  return opposite_coefficient;
+}
 
 int main(){
-  double a, b, c;
+  double a, b, c, num;
   std::cout << "Enter a b c\n";
   std::cin >> b >> a >> c;
+  try{
   double quad = q_pos(b, a, c);
-  std::cout << quad << "\n";
+  //std::cout << quad << "\n";
+  num = numerator(b);
+  double result = num / (2 * a);
+  std::cout << result << "\n";
+  }
+  catch(std::runtime_error& e){
+    std::cerr << "Runtime error: " << e.what() << '\n';
+  }
 
 }
