2388f36b37ab2e3d3aeeb7f70427cdc3b11093f1
ShawnAli97 <shawn10ali@gmail.com>
2024-02-26 02:21:22 -0500

    update

--- totalReview/ch5/exercise/days_values.cpp
+++ totalReview/ch5/exercise/days_values.cpp
@@ -8,41 +8,37 @@
 
 std::vector<std::string> days = {"Monday", "monday", "mon", "mon", "Tueday", "tuesday", "tues", "Tues", "Wendsday", "wendsday", "weds", "Weds", "Thursday", "thursday", "turs", "Thurs", "Friday", "friday", "fri", "Fri", "Saturday", "saturday", "sat", "Sat", "Sunday", "sunday", "sun", "Sun"};
 
-void check_invalid_string_input(std::string s){
-  if(s == std::string(s)){
-    //Convert the string to a int and check if its an int. If not then change it back to a string
-    int number = std::stoi(s);
-    std::cout << number << "\n";
-    if(number == int(number))
-      error("You entered a number, should be a string\n");
-    else{
-      s = std::to_string(number);
-    }
-  }
-  else
-    std::cout << "Nope\n";
-}
-
 void input_days(){
   std::string day;
   while(true){
     std::cin >> day;
+    if(day == "q"){
+      //days.pop_back();
+      break;
+    }  
     for(int i = 0; i < days.size(); ++i){
       //write logic here to iterate over the whole vector for any matches if not then print invalid input and take inpunt again and repeat. 
       if(day != days[i]){
-	std::cout << "Invalid input\n";
-	std::cin >> day;
+	if(i == days.size()-1){
+	  std::cout << "Invalid input\n";
+	  break;
+	}
+
+      }else{
+	std::cout << "Match found\n";
+	break;
       }
     }
-    if(!std::cin)
+    /*if(!std::cin)
       error("Not a string value\n");
     else
       days.push_back(day);
+    */
 
     if(day == "q"){
       days.pop_back();
       break;
-    }
+    }  
   }
 }
 
